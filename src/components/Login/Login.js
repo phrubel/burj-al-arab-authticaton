d83df7dbdeef7663fd9cab73fe0d9e7efc9bcac4
phrubel <phrubel42@gmail.com>
2021-03-17 19:53:45 +0600

    added authtication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,45 @@
 import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
+
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 
 
 const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext)
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(googleProvider)
+            .then((result) => {
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email }
+                setLoggedInUser(signedInUser);
+                history.replace(from)
+
+            }).catch((error) => {
+                var errorMessage = error.message;
+                console.log(errorMessage)
+                // ...
+            });
+
+    }
     return (
         <div>
-            <h1>This is Login</h1>
+            <h1>User Name: {loggedInUser.name}</h1>
+            <h5>User Email: {loggedInUser.email}</h5>
+            <button onClick={handleGoogleSignIn}>Sign in</button>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-FVUpPHF41mJ8IF96vxhPWFPYnfZmPaY",
+    authDomain: "burj-al-arab-571c2.firebaseapp.com",
+    projectId: "burj-al-arab-571c2",
+    storageBucket: "burj-al-arab-571c2.appspot.com",
+    messagingSenderId: "284693488452",
+    appId: "1:284693488452:web:e439c35d34f91ea0cbad11"
+  };
+
+  export default firebaseConfig;
